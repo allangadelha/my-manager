4d372498cecf763d55fe97fbb10e5e689f2a5a9c
Allan Gadelha <allangadelha@gmail.com>
2022-06-02 19:08:52 -0300

    NavBar: style

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,15 +1,24 @@
 import { Link } from "react-router-dom";
 
+import Container from "./Container";
+
+import styles from './NavBar.module.css';
+import logo from '../../img/costs_logo.png';
+
 function NavBar() {
 
     return(
     
-        <div>
-          <Link to='/'>Home</Link>
-          <Link to='/company'>Empresa</Link>
-          <Link to='/contact'>Contato</Link>
-          <Link to='/newproject'>Novo projeto</Link>
-        </div>
+        <nav className={styles.navbar}>
+            <Container>
+                <Link to='/'><img src={logo} alt="My Manager"/></Link>
+                <ul className={styles.list}>
+                    <li className={styles.item}><Link to='/'>Home</Link></li>
+                    <li className={styles.item}><Link to='/company'>Empresa</Link></li>
+                    <li className={styles.item}><Link to='/contact'>Contato</Link></li>
+                </ul>
+            </Container>
+        </nav>
 
     )
 
