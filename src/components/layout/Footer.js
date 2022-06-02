7172d6e214a59322d31d32ebb0988bf9b5a3baa3
Allan Gadelha <allangadelha@gmail.com>
2022-06-02 20:32:39 -0300

    Concluding home; creating component ButtonLink

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -8,9 +8,9 @@ function Footer() {
     
         <footer className={styles.footer}>
             <ul className={styles.social_list}>
-                <li><a target="_blank" rel="norefrrer" href="https://www.facebook.com/allan.gadelhaxavier"><FaFacebook /></a></li>
-                <li><a target="_blank" rel="norefrrer" href="https://www.instagram.com/allangadelhaxavier"><FaInstagram /></a></li>
-                <li><a target="_blank" rel="norefrrer" href="https://www.linkedin.com/in/allan-gadelha-xavier-1659b428"><FaLinkedin /></a></li>
+                <li><a rel="norefrrer" href="https://www.facebook.com/allan.gadelhaxavier"><FaFacebook /></a></li>
+                <li><a rel="norefrrer" href="https://www.instagram.com/allangadelhaxavier"><FaInstagram /></a></li>
+                <li><a rel="norefrrer" href="https://www.linkedin.com/in/allan-gadelha-xavier-1659b428"><FaLinkedin /></a></li>
             </ul>
             <p className={styles.copy_right}>
                 <span>
