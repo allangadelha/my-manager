0c85639bda98716400627b62e6f33b15eeff4b5e
Allan Gadelha <allangadelha@gmail.com>
2022-06-02 16:55:01 -0300

    Creating pages components and routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import Home from './components/pages/Home';
+import NewProject from './components/pages/NewProject';
+
 function App() {
   return (
     <div className="App">
-      <p>My Manager</p>
+      <Router>
+        <div>
+          <Link to='/'>Home</Link>
+          <Link to='/company'>Empresa</Link>
+          <Link to='/contact'>Contato</Link>
+          <Link to='/newproject'>Novo projeto</Link>
+        </div>
+        <Routes>
+          <Route path='/' exact='true' element={<Home />}></Route>
+          <Route path='/company' element={<Company />}></Route>
+          <Route path='/contact' element={<Contact />}></Route>
+          <Route path='/newproject' element={<NewProject />}></Route>
+        </Routes>
+        <p>Footer</p>
+      </Router>
     </div>
   );
 }
