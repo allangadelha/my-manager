4718b5315b77b4e947041c65d51752e82ca4847e
Allan Gadelha <allangadelha@gmail.com>
2022-06-02 20:04:37 -0300

    Footer: style; new page component Projects

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Company from './components/pages/Company';
+import Projects from './components/pages/Projects';
 import Contact from './components/pages/Contact';
 import Home from './components/pages/Home';
 import NewProject from './components/pages/NewProject';
@@ -17,6 +18,7 @@ function App() {
         <Container customClass="min-height">
           <Routes>
             <Route path='/' exact='true' element={<Home />}></Route>
+            <Route path='/projects' element={<Projects />}></Route>
             <Route path='/company' element={<Company />}></Route>
             <Route path='/contact' element={<Contact />}></Route>
             <Route path='/newproject' element={<NewProject />}></Route>
--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,10 +1,23 @@
+import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa';
+
+import styles from './Footer.module.css';
+
 function Footer() {
 
     return(
     
-        <div>
-            <p>Footer</p>
-        </div>
+        <footer className={styles.footer}>
+            <ul className={styles.social_list}>
+                <li><a target="_blank" rel="norefrrer" href="https://www.facebook.com/allan.gadelhaxavier"><FaFacebook /></a></li>
+                <li><a target="_blank" rel="norefrrer" href="https://www.instagram.com/allangadelhaxavier"><FaInstagram /></a></li>
+                <li><a target="_blank" rel="norefrrer" href="https://www.linkedin.com/in/allan-gadelha-xavier-1659b428"><FaLinkedin /></a></li>
+            </ul>
+            <p className={styles.copy_right}>
+                <span>
+                    &copy; { new Date().getFullYear() }
+                </span>
+            </p>
+        </footer>
     )
 
 }
