4b1b1f69b7260765f9c869a9242c57fb80cdcbe9
Allan Gadelha <allangadelha@gmail.com>
2022-06-04 19:54:29 -0300

    Creating project dashboard

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,6 +1,10 @@
 import { useLocation } from "react-router-dom";
 
 import Message from "../layout/Message";
+import Container from "../layout/Container";
+import LinkButtom from "../layout/LinkButton";
+
+import styles from  './Projects.module.css';
 
 function Projects() {
 
@@ -13,9 +17,15 @@ function Projects() {
     
     return (
 
-        <div>
-            <h1>Meus Projetos</h1>
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>Meus Projetos</h1>
+                <LinkButtom to="/newproject" text="Novo Projeto"/>
+            </div>
             {message && <Message type="success" msg="Mensagem de teste" />}
+            <Container customCassl="start">
+                <p>Projetos...</p>
+            </Container>
         </div>
 
     )
