5c0238aee34bf0a68d878fe6a28cafbf621c26d4
Allan Gadelha <allangadelha@gmail.com>
2022-06-06 10:47:18 -0300

    Concluding component Loading

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -5,45 +5,50 @@ import Message from "../layout/Message";
 import Container from "../layout/Container";
 import LinkButtom from "../layout/LinkButton";
 
-import styles from  './Projects.module.css';
+import styles from './Projects.module.css';
 import ProjectCard from "../project/ProjectCard";
+import Loading from "../layout/Loading";
 
 function Projects() {
 
     const [projects, setProjects] = useState([]);
+    const [removeLoading, setRemoveLoading] = useState(false);
 
     const location = useLocation();
     let message = '';
 
-    if(location.state) {
+    if (location.state) {
         message = location.state.message
     }
 
     useEffect(() => {
-        fetch('http://localhost:5000/projects', {
-            method: "GET",
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-        .then((resp) => resp.json())
-        .then((data) => {
-            console.log(data)
-            setProjects(data)
-        })
-        .catch((err) => console.log(err));
+        setTimeout(() => {
+            fetch('http://localhost:5000/projects', {
+                method: "GET",
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then((resp) => resp.json())
+                .then((data) => {
+                    console.log(data)
+                    setProjects(data)
+                    setRemoveLoading(true)
+                })
+                .catch((err) => console.log(err));
+        }, 300);
     }, [])
-    
+
     return (
 
         <div className={styles.project_container}>
             <div className={styles.title_container}>
                 <h1>Meus Projetos</h1>
-                <LinkButtom to="/newproject" text="Novo Projeto"/>
+                <LinkButtom to="/newproject" text="Novo Projeto" />
             </div>
             {message && <Message type="success" msg="Mensagem de teste" />}
             <Container customCassl="start">
-                {projects.length > 0 && 
+                {projects.length > 0 &&
                     projects.map((project) => (
                         <ProjectCard
                             key={project.id}
@@ -52,7 +57,8 @@ function Projects() {
                             valueProject={project.valueProject}
                             category={project.category.name}
                         />
-                ))}
+                    ))}
+                {!removeLoading && <Loading />}
             </Container>
         </div>
 
