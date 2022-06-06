c4f3196a60af05fd3d9f4f198d9262b45a6135e8
Allan Gadelha <allangadelha@gmail.com>
2022-06-06 09:21:38 -0300

    Concluding style Projects: style in buttons

--- src/components/project/ProjectCard.js
+++ src/components/project/ProjectCard.js
@@ -1,5 +1,6 @@
 import styles from './ProjectCard.module.css';
-import { BsPencil, BsFillTrashFill } from 'react-icons/bs';
+import { BsPencil, BsFillTrashFill, BsTrashFill } from 'react-icons/bs';
+import { Link } from 'react-router-dom';
 
 function ProjectCard({ id, name, valueProject, category, handleRemove }) {
 
@@ -12,9 +13,13 @@ function ProjectCard({ id, name, valueProject, category, handleRemove }) {
             <p className={styles.category_text}>
                 <span className={`${styles[category.toLowerCase()]}`}></span> {category}
             </p>
-            <div>
-                <p>Editar</p>
-                <p>Remover</p>
+            <div className={styles.project_card_actions}>
+                <Link to='/'>
+                    <BsPencil /> Editar
+                </Link>
+                <button>
+                    <BsTrashFill /> Remover
+                </button>
             </div>
         </div>
         
