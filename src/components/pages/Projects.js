317d594fbd68e0340fd60ae4641137dec7843995
Allan Gadelha <allangadelha@gmail.com>
2022-06-06 09:04:31 -0300

    Creating component ProjectCard with style

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,12 +1,16 @@
 import { useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 import Message from "../layout/Message";
 import Container from "../layout/Container";
 import LinkButtom from "../layout/LinkButton";
 
 import styles from  './Projects.module.css';
+import ProjectCard from "../project/ProjectCard";
 
 function Projects() {
+
+    const [projects, setProjects] = useState([]);
 
     const location = useLocation();
     let message = '';
@@ -14,6 +18,21 @@ function Projects() {
     if(location.state) {
         message = location.state.message
     }
+
+    useEffect(() => {
+        fetch('http://localhost:5000/projects', {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then((resp) => resp.json())
+        .then((data) => {
+            console.log(data)
+            setProjects(data)
+        })
+        .catch((err) => console.log(err));
+    }, [])
     
     return (
 
@@ -24,7 +43,16 @@ function Projects() {
             </div>
             {message && <Message type="success" msg="Mensagem de teste" />}
             <Container customCassl="start">
-                <p>Projetos...</p>
+                {projects.length > 0 && 
+                    projects.map((project) => (
+                        <ProjectCard
+                            key={project.id}
+                            id={project.id}
+                            name={project.name}
+                            valueProject={project.valueProject}
+                            category={project.category.name}
+                        />
+                ))}
             </Container>
         </div>
 
---
+++ src/components/project/ProjectCard.js
@@ -0,0 +1,26 @@
+import styles from './ProjectCard.module.css';
+import { BsPencil, BsFillTrashFill } from 'react-icons/bs';
+
+function ProjectCard({ id, name, valueProject, category, handleRemove }) {
+
+    
+    return (
+
+        <div className={styles.project_card}>
+            <h4>{name}</h4>
+            <p><span>Orçamento: </span> R$ {valueProject}</p>
+            <p className={styles.category_text}>
+                <span className={`${styles[category.toLowerCase()]}`}></span> {category}
+            </p>
+            <div>
+                <p>Editar</p>
+                <p>Remover</p>
+            </div>
+        </div>
+        
+
+    )
+
+}
+
+export default ProjectCard;
